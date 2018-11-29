bd4754d494fd794e47fc4c6c59dab104b85fd959
Josh Koiro <josh.koiro@gmail.com>
2018-11-28 22:09:20 -0500

    added pug views and bootstrap

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//TEMPLATE PARAMETERS --------------------------------------------------------------
+//TEMPLATE CONFIGURATION PARAMETERS --------------------------------------------------------------
 
 //set up parameters to be tracked
 //Parameter Names must have no spaces!
@@ -12,21 +12,43 @@ var fileLoc = "data.csv"
 
 //-------------------------------------------------------------------------
 
+//IMPORT DEPENDENCIES -----------------------------------------------------
+
 //set up express server
 var express = require('express');
 var app = express();
+
+//import file system and os components
 var fs = require('fs');
 var os = require('os');
+var path = require('path');
+
+//import csv parsing components
 var csv = require('fast-csv');
 
-//data to be stored in memory (temporary)
+//import http server components
+var http = require('http').Server(app)
+
+//import socket.io components
+var io = require('socket.io')(http);
+
+//--------------------------------------------------------------------------
+
+//PREPARE .CSV FILE DATABASE -----------------------------------------------
+
+//data to be stored in memory
 var data = []
 
 //Check if data file exists. If so, import. If not, create one.
 fs.exists(fileLoc,(exist) => {
     if(exist){
+
+        //Case that data file with specified name from variable fileLoc does exist in directory
+        
         console.log("Loading Data File")
-            var parser = csv.fromPath(fileLoc,{headers:true})
+            
+        //Using fast-csv to parse the existing csv file
+        var parser = csv.fromPath(fileLoc,{headers:true})
             .on("data",function(csvData){
                 data.push(csvData)
             })
@@ -34,8 +56,10 @@ fs.exists(fileLoc,(exist) => {
                 console.log('Data from ' + fileLoc + ' successfully loaded....listening for data entry')
             })
     } 
-    else {
-        
+    else { 
+
+        //Case that there is no .csv file with name specified by variable fileLoc in directory
+
         console.log ('Creating ' + fileLoc + ' as data file')
         
         //format header row in csv file
@@ -49,16 +73,35 @@ fs.exists(fileLoc,(exist) => {
     }
 })
 
-//concat string to add to parameters to the routing routine
+//--------------------------------------------------------------------------
+
+//take the parameters in config array and join them together as string for express server routing with parameters
 var serverCall = ":" + parameters.join("/:")
+
+//set headers variable
+let headers = parameters
+    headers.push('timeStamp')
 
 //set date variable
 var currentDate = new Date()
 
-//home routing
+//Setup view engine for html template files
+app.set("view engine", "pug");
+
+//Setup Express to look at views folder for pug files
+app.set("views",path.join(__dirname, "views"));
+
+//Load Dashboard on Client
 app.get('/', function (req, res) {
-   res.header("Content-Type","text/plain")
-   res.send('data server is running. to add data use: \r\n\r\n"/write/[' + parameters.join("]/[") + ']"' + '\r\n\r\n\r\n' + 'there are currently: ' + data.length + ' records in dataset.');
+    res.render("dashboard",{})
+})
+
+//Load Tabular Data on Client
+app.get('/table', function (req, res) {
+    res.render("table",{
+        headers:parameters,
+        data:data
+    })
 })
 
 //write data to data file routing
@@ -97,8 +140,19 @@ app.get('/read', function (req, res) {
  })
 
 
-//run express server 
-var server = app.listen(3000, function () {
+ //Socket io listeners
+
+ io.on('connection',() => {
+     console.log('client connected')
+ })
+
+ io.on('disconnect',() => {
+     console.log('client disconnected')
+ })
+
+
+//run server 
+var server = http.listen(3000, function () {
    var host = server.address().address
    var ipAddress = os.networkInterfaces()[Object.keys(os.networkInterfaces())[0]][1].address
    var port = server.address().port
