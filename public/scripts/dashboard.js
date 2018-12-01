b116f84006f61ca9e5ac579ea8039a3e10a8ef40
Josh Koiro <josh.koiro@gmail.com>
2018-12-01 09:25:02 -0500

    last data point entered card refreshes on data entry

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,8 +1,13 @@
 $(() => {
+
+    var refreshTimes = window.setInterval(refresh,60000)
+
     var socket = io();
 
     socket.on('update data', (data) => {
-
+        //update values on dashboard
         $("#dataLength").text(data.dataLength)
+        $("#lastDataTime").text(moment(data.timeStamp).startOf("minute").fromNow())
+        
     })
 })
